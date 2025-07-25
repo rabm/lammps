e215cf541258c1d380461562e336c9efa44edf91
Danny van der Haven <dannyvdhaven@gmail.com>
2025-07-25 20:17:47 +0200

    Polishing, removing redundancies, minor optimisation

--- src/LSDEM/fix_rigid_ls_dem.cpp
+++ src/LSDEM/fix_rigid_ls_dem.cpp
@@ -3227,18 +3227,14 @@ double FixRigidLSDEM::get_ls_value(int i, int j, double *normal)
   double **x = atom->x;
   double **grain_com = atom->darray[index_ls_dem_com];
   double **grain_quat = atom->darray[index_ls_dem_quat];
-  double **grain_grid = atom->darray[index_ls_grid];
+  double **grain_grid = atom->darray[index_ls_grid]; // Danny: This is per atom/node, so I would call it node_grid.
   double **local_grid_min = atom->darray[index_ls_local_gridmin];
 
-  int ncol = ngrid_local[0];
+  int ncol = ngrid_local[0]; // Danny: this should be for the node's grid as well
   int nrow = ngrid_local[1];
   int nslice = ngrid_local[2];
 
-  //int nrow_offset = local_grid_min[j][0]; // Offsets for local subgrid
-  //int ncol_offset = local_grid_min[j][1];
-  //int nslice_offset = local_grid_min[j][2];
-
-  // Calculate position of i in j's grid using:
+  // Calculate position of node i in node j's grid using:
   //   x[i][0-2] = location of i
   //   x[j][0-2] = location of j
   //   grain_com[j][0-2] = CoM of j's grain
@@ -3248,68 +3244,49 @@ double FixRigidLSDEM::get_ls_value(int i, int j, double *normal)
   //  GET NODE I IN LOCAL COORDINATES OF J GRAIN
   //
 
-  // location of atom/node relative to CoM
+  // Location of the node (atom) of i relative to the centre of mass (CoM) of j.
   double delx = x[i][0] - grain_com[j][0];
   double dely = x[i][1] - grain_com[j][1];
   double delz = x[i][2] - grain_com[j][2];
 
-  // Account for PBCs
+  // Account for PBCs.
   domain->minimum_image(delx, dely, delz);
 
-  // Extract quaternion components
-  // Danny: How is grain_quat defined? Is it the rotation local -> global or global -> local?
-  // I will assume it is local -> global
-  // Remove minus signs if grain_quat is global -> local
-  //double q_w = grain_quat[j][0];
-  //double q_x = -grain_quat[j][1];
-  //double q_y = -grain_quat[j][2];
-  //double q_z = -grain_quat[j][3];
-
-  // Apply quaternion rotation to move into local reference frame of grain j grid
-  // x' = (q*x)*q^-1 // Joel: this code might have a typo so I replaced it with the method below
-  //double x_local = (1 - 2 * (q_y * q_y + q_z * q_z)) * delx + 2 * (q_x * q_y - q_w * q_z) * dely + 2 * (q_x * q_z + q_w * q_y) * delz;
-  //double y_local = 2 * (q_x * q_y + q_w * q_z) * delx + (1 - 2 * (q_x * q_x + q_z * q_z)) * dely + 2 * (q_y * q_z - q_w * q_x) * delz;
-  //double z_local = 2 * (q_x * q_z - q_w * q_y) * delx + 2 * (q_y * q_z + q_w * q_x) * dely + (1 - 2 * (q_x * q_x + q_y * q_y)) * delz;
-
-  // sample code with math_extra, feel free to add MathExtra to namespace if helpful
+  // Apply quaternion rotation to move into local reference frame of grain j grid.
+  // Here, grain_quat is local->global. Therefore, grain_quat_conj is global -> local.
   double x_local[3];
   double dx[3] = {delx, dely, delz};
-
-  // grain_quat is local->global I think
   double grain_quat_conj[4];
   MathExtra::qconjugate(grain_quat[j], grain_quat_conj);
-  MathExtra::quatrotvec(grain_quat_conj, dx, x_local); // I think it's global -> local, but if you need to take a conjugate there's a function qconjugate()
-
-  // see comments above functions in math_extra.h/cpp for details
+  MathExtra::quatrotvec(grain_quat_conj, dx, x_local);
+  // See comments above functions in math_extra.h/cpp for details
 
   //
   //  COMPUTE THE LS GRID INDICES
   //
 
+  // Translate local coordinates such that they are relative 
+  // to the lower corner of the level set grid.
   x_local[0] -= local_grid_min[j][0];
   x_local[1] -= local_grid_min[j][1];
   x_local[2] -= local_grid_min[j][2];
+  // Danny: THIS local_grid_min NEEDS TO BE THE MINIMUM OF THE NODE'S GRID, NOT THE FULL GRID!
 
-  // Calculate index from coordinate, need to be more careful with integer division
-  // Danny: We need to get grid_min, the lowest corner (in -1,-1,-1 direction) of the grid
-  //        and spac, the grid spacing. (If we want to keep this in normalised coords, we
-  //        will have to normalise.)
+  // Normalise the coordinates to be in units of the number of grid cells.
+  double x_red = x_local[0] / spac;
+  double y_red = x_local[1] / spac;
+  double z_red = x_local[2] / spac;
+
+  // Calculate index from relative coordinate, being careful with integer division.
+  int ind_x = int(x_red); // Here, int() does the same as floor() + conversion.
+  int ind_y = int(y_red);
+  int ind_z = int(z_red); // Should always be zero in 2D.
   // JBC: There is some padding for detection / normal caculation that I don't understand clearly
+  // Danny: Does the below clarify? Or is there something else that is missing?
 
-  int ind_x = int(x_local[0] / spac); // Here, int() does the same as floor() + conversion
-  int ind_y = int(x_local[1] / spac);
-  int ind_z = int(x_local[2] / spac); // Should always be zero in 2D
-
-  // Apply local offsets
-  //ind_x = ind_x - nrow_offset;
-  //ind_y = ind_y - ncol_offset;
-  //ind_z = ind_z - nslice_offset;
-
-  // We might need an extra check. If x_local is very close to grid_min, it may pass and give
-  // errors later.
-  // Joel: added +/- for interpolation
-
-  if ( (ind_x < 1) || (ind_y < 1) || ((domain->dimension == 3) && (ind_z < 1)) ) {
+  // Checking whether x_local lies within the grid. Avoids edge cases where finite precision
+  // leads to e.g. a x=-0.1 coordinate to fall outside of a grid that starts at x=-0.1.
+  if ( (ind_x < 0) || (ind_y < 0) || ((domain->dimension == 3) && (ind_z < 0)) ) {
     // Point is outside the LS grid of grain j. Cannot compute distance or normal.
     error->one(FLERR, "Contacting node {} is outside of node {}'s LS grid", atom->tag[i], atom->tag[j]);
   } else if ( (ind_x >= nrow - 1) || (ind_y >= ncol - 1) || ((domain->dimension == 3) && (ind_z >= nslice - 1)) ) {
@@ -3322,11 +3299,11 @@ double FixRigidLSDEM::get_ls_value(int i, int j, double *normal)
   //
   double dist, nx(0.0), ny(0.0), nz(0.0);
 
-  // The reduced coordinates
-  // May be safe to cap them with math::max(math::min(x_red, 1.0), 0.0)
-  double x_red = x_local[0] / spac - static_cast<double>(ind_x);
-  double y_red = x_local[1] / spac - static_cast<double>(ind_y);
-  double z_red = x_local[2] / spac - static_cast<double>(ind_z); // should always be zero in 2D
+  // The normalised coordinates within the current grid cell.
+  // May be safer to cap them with math::max(math::min(x_red, 1.0), 0.0)
+  x_red = x_red - static_cast<double>(ind_x);
+  y_red = y_red - static_cast<double>(ind_y);
+  z_red = z_red - static_cast<double>(ind_z); // Should always be zero in 2D.
 
   // Level-set values on the grid points in the lower z plane (ind_z)
   double ls000 = grain_grid[j][ind_x   + ind_y     * ncol + ind_z * ncol * nrow];
@@ -3338,7 +3315,8 @@ double FixRigidLSDEM::get_ls_value(int i, int j, double *normal)
   double lsxy0 = ls000 + y_red * (ls010 - ls000) +
                          x_red * (ls100 - ls000 +
                                   y_red * (ls110 - ls100 - ls010 + ls000));
-  if (domain->dimension == 3) {
+
+  if (domain->dimension == 3) { // 3D
     // Level-set values on the grid points in the upper z plane (ind_z+1)
     double ls001 = grain_grid[j][ind_x   + ind_y     * ncol + (ind_z+1) * ncol * nrow];
     double ls101 = grain_grid[j][ind_x+1 + ind_y     * ncol + (ind_z+1) * ncol * nrow];
@@ -3350,15 +3328,11 @@ double FixRigidLSDEM::get_ls_value(int i, int j, double *normal)
                            x_red * (ls101 - ls001 +
                                     y_red * (ls111 - ls101 - ls011 + ls001));
 
-    // Tri-linear interpolation
+    // Affecting tri-linear interpolation by linear interpolation of the two bi-linear interpolations.
     dist = z_red * (lsxy1 - lsxy0) + lsxy0;
-    // JBC: The former code below had this `/spac` that was not in the lower z plane of the 2D code
-    //      I think it was a mistake? I removed it from the more compact expression above
-    // double term = y_red * (ls111 - ls101 - ls011 + ls001) + ls101 - ls001;
-    // double lsxy1 = x_red / spac * term + y_red * (ls011 - ls001) + ls001;
 
-	// Computing normal as the gradient of trilinear interpolation
-    // TODO: maybe hardcode without loops ?
+	  // Computing normal as the gradient of trilinear interpolation
+    // TODO: maybe hardcode without loops ? Danny: Compiler should optimise this automatically, don't think it changes anything?
     for (int a = 0; a < 2; a++) {
       for (int b = 0; b < 2; b++) {
         for (int c = 0; c < 2; c++) {
@@ -3369,11 +3343,10 @@ double FixRigidLSDEM::get_ls_value(int i, int j, double *normal)
         }
       }
     }
-  } else {
+  } else { // 2D
     // Bi-linear interpolation
     dist = lsxy0;
     // Computing normal as the gradient of bilinear interpolation
-    // TODO: maybe hardcode without loops ?
     for (int a = 0; a < 2; a++) {
       for (int b = 0; b < 2; b++) {
         double lsVal = grain_grid[j][(ind_x + a) + (ind_y + b) * ncol];
@@ -3394,3 +3367,5 @@ double FixRigidLSDEM::get_ls_value(int i, int j, double *normal)
 
   return dist;
 }
+
+// End of file
